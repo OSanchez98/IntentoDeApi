a678ad2ecb76c800b960bd7adb7309ec99956a77
OSanchez98 <32837879+OSanchez98@users.noreply.github.com>
2018-07-05 15:47:43 -0500

    ya app puerto 300

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var path = require('path');
+
 var app = express();
 
 app.use(bodyParser.json());
